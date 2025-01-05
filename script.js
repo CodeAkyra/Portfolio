901ca3cefbd5d8ef7a385ca83c841120de217c99
qwerty <93528492+CodeAkyra@users.noreply.github.com>
2025-01-05 18:13:22 +0800

    javascript updated

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var header = document.getElementById("nav-content");
+var header = document.getElementById("nav-cont");
 var btns = header.getElementsByClassName("buttons");
+
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
     this.className += " active";
   });
 }
